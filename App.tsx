9865dea4bab3b07d08938a761dc7fa5fe61f5b1a
webcuser <97110714+webcuser@users.noreply.github.com>
2026-07-09 16:28:40 +0200

    Initialize Expo project with TypeScript, Redux Toolkit, React Navigation, and AsyncStorage

---
+++ App.tsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+import { Provider } from 'react-redux';
+import { store } from './src/store';
+import HomeScreen from './src/screens/HomeScreen';
+
+const Stack = createStackNavigator();
+
+export default function App() {
+  return (
+    <Provider store={store}>
+      <NavigationContainer>
+        <Stack.Navigator>
+          <Stack.Screen name="Home" component={HomeScreen} />
+        </Stack.Navigator>
+      </NavigationContainer>
+    </Provider>
+  );
+}
